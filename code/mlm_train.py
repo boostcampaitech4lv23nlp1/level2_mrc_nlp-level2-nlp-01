fa63624d742789d878404e0cefe5765d9d7fe229
wooy0ng <yygg9800@naver.com>
2023-01-03 10:09:51 +0000

    feat: add mlm training script (#4)

--- code/mlm_train.py
+++ code/mlm_train.py
@@ -1,10 +1,8 @@
 import logging
 import os
-import sys
 from typing import NoReturn
-import shutil
-import utils
 import wandb
+import shutil
 
 from omegaconf import OmegaConf
 from arguments import DataTrainingArguments, ModelArguments
@@ -27,12 +25,20 @@ logger = logging.getLogger(__name__)
 
 
 def main(cfg):
+    if cfg.train.path.delete_exist_output is True:
+        if os.path.exists(cfg.train.path.output_dir):
+            flag = input(f"정말 {cfg.train.path.output_dir} 내의 모든 파일을 삭제하시겠습니까? (yes) >> ")
+            if flag == 'yes':
+                shutil.rmtree(cfg.train.path.output_dir)
+            else:
+                print('기존 output을 삭제하지 않습니다.')
     '''
         TODO 1: 데이터셋을 불러옵니다.
             1) 기존 데이터셋
             2) korquad 데이터셋
     '''
     datasets = load_dataset('squad_kor_v1')
+    # datasets = load_from_disk(cfg.train.path.dataset_name)
     print(datasets)
 
     column_names = datasets['train'].column_names
@@ -81,14 +87,15 @@ def main(cfg):
         anony = "must"
         print('If you want to use your W&B account, go to Add-ons -> Secrets and provide your W&B access token. Use the Label name as wandb_api. \nGet your W&B access token from here: https://wandb.ai/authorize')
 
-    wandb.init(project="project", name= "test")
+    wandb.init(project="pretraining", name= "korquad-training")
 
     training_args = TrainingArguments(
         output_dir=cfg.train.path.output_dir,
         evaluation_strategy='epoch',
         learning_rate=2e-5,
-        num_train_epochs=3,
+        num_train_epochs=1,
         weight_decay=0.01,
+        save_strategy='epoch',
         report_to='wandb'
     )
 
@@ -101,12 +108,29 @@ def main(cfg):
     )
 
     print('pretraining start')
-    trainer.train()
+    train_result = trainer.train(
+        resume_from_checkpoint=None
+    )
 
+    print('save model')
+    trainer.save_model()
+
+    metrics = train_result.metrics
+    metrics['train_samples'] = len(train_datasets)
+
+    tokenizer.save_pretrained(cfg.train.path.output_dir)
+
+    trainer.log_metrics('train', metrics)
+    trainer.save_metrics('train', metrics)
+    trainer.save_state()
+    
+    trainer.state.save_to_json(
+        os.path.join(cfg.train.path.output_dir, 'trainer_state.json')
+    )
     
 if __name__ == "__main__":
     # configuation
-    config_name = 'roberta-large_config'
-    cfg = OmegaConf.load(f'./conf/reader/{config_name}.yaml')
+    config_name = 'base_config'
+    cfg = OmegaConf.load(f'./conf/MLM/{config_name}.yaml')
 
     main(cfg)
